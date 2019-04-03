3fb1114d0b4e33f99f991ef25b7933293b6e866c
daothanhlong11 <1611853@hcmut.edu.vn>
2019-04-04 04:37:54 +0700

    update positon part and add convert_int2locate

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from __future__ import print_function
 import numpy as np
-# import pandas as pd
 import csv
+import random
 
-
-def position(height, width, a):
+def position(height, width, number_locate,number_table):
     position_table = np.zeros((height+2, width+2), dtype=int)
-    a_position_row = a[0]+1
-    a_position_col = a[1]+1
+    a_position_row = number_locate[0]+1
+    a_position_col = number_locate[1]+1
     for i in range(height):
         if (i > 0 & i < (height)):
             for j in range(width):
@@ -20,14 +19,23 @@ def position(height, width, a):
                    (a_position_col-1):(a_position_col+2)] = all_move + position_filter
     p = np.where(position_table == 1)
     position = []
+    list_position = []
     for i in range((np.shape(p))[1]):
         if (((p[0][i]) > 0) & ((p[0][i]) < 6) & ((p[1][i]) > 0) & ((p[1][i]) < 7)):
             position.append([(p[0][i]-1), (p[1][i]-1)])
+    for value in position:
+        value.append(number_table[value[0],value[1]])
+        list_position.append(number_table[value[0],value[1]])
     position = np.asarray(position)
-    return position
+    return position,list_position
 
+def convert_int2locate(int_number,number_table):
+    l_int = np.where(number_table==int_number)
+    int_locate = np.array([l_int[0][0],l_int[1][0]])
+    return int_locate
 
-def q_function(s, a, posible_move_position):
+#def q_function(s, a, posible_move_position):
+
 
 
     # khoi tao ma tran R-table tu du kien bai toan
@@ -45,10 +53,20 @@ with open("D:\Code_software\VScode\python\DKTM_btl2\R_talbe.csv",'w') as csvfile
 # khoi tao ma tran 0 cua Q learning
 Q_table = np.zeros((height, width), dtype=float)
 
-# tao hai bien state ban dau va action
-s = np.array([1, 1])
-a = np.array([4, 5])
-posible_move_position = position(height, width, a)
+# khoi tao stt cua tung buoc di trong table
+number_table = np.zeros((height,width),dtype=int)
+number = 0
+for i in range(height):
+    for j in range(width):
+        number_table[i,j] = number
+        number+=1
+
+
+# tao bien state ban dau
+s_0 = int(input("nhap stt cua vi tri state ban dau: "))
+s_locate = convert_int2locate(s_0,number_table)
+posible_move_position,list_posible_move = position(height, width, s_locate,number_table)
+
 
 
 print("something")
