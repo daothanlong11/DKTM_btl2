28ba7441c8a793d6a89b74d590b1740bb7ce17b1
daothanhlong11 <1611853@hcmut.edu.vn>
2019-04-05 01:05:09 +0700

    update 2: done building q learning model

--- main.py
+++ main.py
@@ -34,22 +34,27 @@ def convert_int2locate(int_number,number_table):
     int_locate = np.array([l_int[0][0],l_int[1][0]])
     return int_locate
 
-#def q_function(s, a, posible_move_position):
+def Max_score(R_talbe,number_table,a_locate,height,width):
+    list_locate,_ = position(height,width,a_locate,number_table)
+    list_score = []
+    for i in range(len(list_locate)):
+        list_score.append(R_talbe[list_locate[i][0],list_locate[i][1]])
+    max_score = max(list_score)
+    return max_score
 
+def q_function(a_locate,lr,gamma, posible_move_position,Q_table,R_talbe,number_table,height,width):
+    Q_table[a_locate[0],a_locate[1]] = ( Q_table[a_locate[0],a_locate[1]] + lr*( R_talbe[a_locate[0],a_locate[1]] + gamma*(Max_score(R_talbe,number_table,a_locate,height,width)) ) )
+    
 
 
     # khoi tao ma tran R-table tu du kien bai toan
-width = 6
-height = 5
+
 R_talbe = np.array([[0, -1, +1, -1, -1, -1], [-1, -100, -1, -1, -100, -1],
                     [-1, -1, +1, -1, -1, +1], [-100, -1, -1, -100, -1, -1], [-1, +1, -1, -1, +100, -1]])
-"""
-with open("D:\Code_software\VScode\python\DKTM_btl2\R_talbe.csv",'w') as csvfile:
-    write = csv.writer(csvfile,delimiter=',')
-    for i in range(height):
-        write.writerow(R_talbe[i,:])
+height = np.shape(R_talbe)[0]
+width = np.shape(R_talbe)[1]
+print(R_talbe[1,1])
 
-"""
 # khoi tao ma tran 0 cua Q learning
 Q_table = np.zeros((height, width), dtype=float)
 
@@ -61,12 +66,27 @@ for i in range(height):
         number_table[i,j] = number
         number+=1
 
-
-# tao bien state ban dau
+# phan code chay chinh
 s_0 = int(input("nhap stt cua vi tri state ban dau: "))
-s_locate = convert_int2locate(s_0,number_table)
-posible_move_position,list_posible_move = position(height, width, s_locate,number_table)
+s0_locate = convert_int2locate(s_0,number_table)
+posible_move_position,list_posible_move = position(height, width, s0_locate,number_table)
+episode = int(input("nhap so episode muon huan luyen: "))
+lr = float(input("nhap learning rate cho model: "))
+gamma = float(input("nhap discount rate cho model: "))
+for i in range(episode):
+    a = random.choice(list_posible_move)
+    a_locate = convert_int2locate(a,number_table)
+    q_function(a_locate,lr,gamma,posible_move_position,Q_table,R_talbe,number_table,height,width)
+    s_locate = a_locate
+    posible_move_position,list_posible_move = position(height,width,s_locate,number_table)
 
+with open("D:\code\python\DKTM_btl2\R_Q_table.csv",'w') as csvfile:
+    write = csv.writer(csvfile,delimiter=',')
+    for i in range(height):
+        write.writerow(R_talbe[i,:])
+    write.writerow("\n")
+    write.writerow("\n")
+    for i in range(height):
+        write.writerow(Q_table[i,:])
+    csvfile.close()
 
-
-print("something")
